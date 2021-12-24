05b9c8cf31fc6fc3d1336a637d555820b11308ed
Andrey Karelin <kkarelinn@gmail.com>
2021-12-24 09:18:47 +0200

    Clear branch Lab2SushiShop

---
+++ src/main/java/com/example/lab2sushishop/Lab2SushiShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2sushishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SushiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2SushiShopApplication.class, args);
+    }
+
+}
